e945985435ba6f52a766246a4805f91814853906
nizar0rchid <nizar.ferchichi@esprit.tn>
2023-04-26 14:09:49 +0100

    firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:square/core/utils/router.dart';
 
 import 'core/utils/constants.dart';
+import 'firebase_options.dart';
 import 'injection_container.dart' as di;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await di.init();
   runApp(const MyApp());
 }
